4d5bb5b273c0f7cd0ffec9f7b71f7446e87491a6
Thorsten Jung <tajbender@hotmail.de>
2024-02-02 16:31:18 +0100

    add control

--- src/electrifier/Controls/WebBrowserNavigationControl.xaml.cs
+++ src/electrifier/Controls/WebBrowserNavigationControl.xaml.cs
@@ -16,7 +16,7 @@ using Windows.Foundation.Collections;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace electrifier;
+namespace electrifier.Controls;
 public sealed partial class WebBrowserNavigationControl : UserControl
 {
     public WebBrowserNavigationControl()
